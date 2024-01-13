ac713c9d778b5a50bdef06c9baa9b776972d882f
Guts <davithachnazaryan@gmail.com>
2024-01-13 13:36:48 -0800

    dssddsdds

---
+++ app/components/navBar/NavBar.js
@@ -0,0 +1,35 @@
+export default function NavBar() {
+  return (
+    <nav>
+      <h3>Poli</h3>
+
+      <ul>
+        <li>
+          <a href="">Demos</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Services</a>
+        </li>
+        <li>
+          <a href="">Reviews</a>
+        </li>
+        <li>
+          <a href="">Pricing</a>
+        </li>
+        <li>
+          <a href="">All Pages</a>
+        </li>
+      </ul>
+
+      <img src="/shoppingCart.svg" />
+
+      <div className="sign">
+        <button className="signIn">Sign Iffffasdfas</button>
+        <button className="ujfsignUp">Sign Up</button>
+      </div>
+    </nav>
+  );
+}
